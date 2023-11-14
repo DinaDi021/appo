189c51357700ee2f558e4cfa4664e208196556ee
DinaDi025 <di.serdyuck0201@gmail.com>
2023-11-14 21:31:04 +0200

    add forgot link

--- src/components/LoginPanel/Form/ForgotPasswordForm.tsx
+++ src/components/LoginPanel/Form/ForgotPasswordForm.tsx
@@ -2,9 +2,8 @@ import { joiResolver } from "@hookform/resolvers/joi";
 import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
 import { FC } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 
-import { useAppDispatch, useAppSelector } from "../../../hooks";
+import { useAppDispatch, useAppSelector, useToggle } from "../../../hooks";
 import { IForgotPassword } from "../../../interfaces";
 import { authActions } from "../../../redux";
 import { forgotPasswordSchema } from "../../../validators";
@@ -20,47 +19,56 @@ const ForgotPasswordForm: FC = () => {
     resolver: joiResolver(forgotPasswordSchema),
   });
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const { error } = useAppSelector((state) => state.auth);
+  const { value: isFormVisible, change: toggleFormVisibility } =
+    useToggle(true);
 
   const forgotPassword: SubmitHandler<IForgotPassword> = async (data) => {
     const { email } = data;
-    const resetUrl = "https://your-website.com/reset-password";
+    const resetUrl = "https://ocean-beauty.netlify.app/resetPassword";
     const {
       meta: { requestStatus },
     } = await dispatch(authActions.forgotPassword({ email, resetUrl }));
 
     if (requestStatus === "fulfilled") {
       reset();
-      navigate("/main");
+      toggleFormVisibility();
     }
   };
 
   return (
     <>
-      <form
-        className={styles.form__login}
-        onSubmit={handleSubmit(forgotPassword)}
-      >
-        <div className={styles.form__container}>
-          <label className={styles.form__label}>
-            <AlternateEmailOutlinedIcon />
-            <input
-              type="email"
-              placeholder={"Email"}
-              required={true}
-              {...register("email")}
-            />
-          </label>
-          {errors.email && (
-            <div className={styles.form__error}>
-              {errors?.email && <span>invalid email</span>}
-            </div>
-          )}
+      {isFormVisible ? (
+        <form
+          className={styles.form__login}
+          onSubmit={handleSubmit(forgotPassword)}
+        >
+          <div className={styles.form__container}>
+            <label className={styles.form__label}>
+              <AlternateEmailOutlinedIcon />
+              <input
+                type="email"
+                placeholder={"Email"}
+                required={true}
+                {...register("email")}
+              />
+            </label>
+            {errors.email && (
+              <div className={styles.form__error}>
+                {errors?.email && <span>invalid email</span>}
+              </div>
+            )}
+          </div>
+          {error && <span className={styles.errMessage}>{error.message}</span>}
+          <button>Reset the password</button>
+        </form>
+      ) : (
+        <div>
+          <h3 className={styles.emailMessage}>
+            Password reset successful! Please check your email.
+          </h3>
         </div>
-        {error && <span className={styles.errMessage}>{error.message}</span>}
-        <button>Reset the password</button>
-      </form>
+      )}
     </>
   );
 };
