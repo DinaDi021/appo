a5a5adfa6907f3624eb2298478b6fd412d438dd8
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-21 19:44:20 +0300

    contact as master in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,16 @@ import React, { FC } from "react";
 import styles from "./Footer.module.scss";
 
 const Footer: FC = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        Want to register as a master? Contact the admin at{" "}
+        <a href="mailto:di.ilina.work@gmail.com" className={styles.email}>
+          admin@gmail.com
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export { Footer };
