3dfcf76e32d36c3f484739634a2fb782b4f1ca31
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-21 19:21:00 +0300

    add navigate not auth user for page with schedule

--- src/pages/AvailableSchedulesPage/AvailableSchedulesDetailsPage.tsx
+++ src/pages/AvailableSchedulesPage/AvailableSchedulesDetailsPage.tsx
@@ -1,12 +1,32 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { AvailableSchedulesDetails } from "../../components";
+import { AvailableSchedulesDetails, IsLoading } from "../../components";
 import { GalleryForClient } from "../../components/Gallery/GalleryForClient";
-import styles from "../../components/Schedules/AllSchedules/AllSchedules.module.scss";
-import { useAppSelector } from "../../hooks";
+import styles from "../../components/Schedules/AvailableSchedules/AvailableSchedules.module.scss";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { authActions } from "../../redux";
 
 const AvailableSchedulesDetailsPage: FC = () => {
   const { selectedMaster } = useAppSelector((state) => state.schedules);
+  const { user } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(
+        authActions.setError({
+          message: "You need to be logged in to access this page.",
+        }),
+      );
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  if (!user) {
+    return <IsLoading />;
+  }
   return (
     <div className={styles.available__page}>
       <AvailableSchedulesDetails availableSchedule={selectedMaster} />
